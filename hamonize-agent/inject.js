1b4564392329e3d135705bdd77c133d9edd8b539
jullee96 <bono623@naver.com>
2021-06-01 18:53:27 +0900

    edit source

--- hamonize-agent/inject.js
+++
@@ -1,7 +0,0 @@
-// node api 
-const  { ipcRenderer }  =  require ( 'electron' )
-
-ipcRenderer . on ( 'getContent' ,  function () { 
-  var  content  =  document . getElementsById ( "content" ); 
-  ipcRenderer . sendToHost ( 'getContent' ,  content . innerText ); 
-});
---
+++ hamonize-agent/src/shell/progrmjob.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+centerUrl=`cat /etc/hamonize/propertiesJob/propertiesInfo.hm | grep CENTERURL | awk -F'=' '{print $2}'`
+CENTERURL="http://${centerUrl}/hmsvc/updtpolicy"
+PCUUID=`cat /etc/hamonize/uuid`
+LOGFILE="/var/log/hamonize/agentjob/progrmjob.log"
+touch $LOGFILE
+
+
+
+echo "################### updt deb install ###########################"
+
+UPDT_INS=`cat /etc/hamonize/progrm/progrm.hm | jq '.INS' | sed -e "s/\"//g" ` 
+echo "install file total  data=========$UPDT_INS" >>$LOGFILE
+
+if [ "$UPDT_INS" != null ] 
+then
+        echo "program access unpossible ==" >>$LOGFILE
+
+EC=0
+INSRET=""
+OLD_IFS=$IFS;
+IFS=,;
+for I in $UPDT_INS;
+do
+
+
+        echo "program  name is ===> $I" >>$LOGFILE
+	
+
+	sudo ps aux | grep $I | awk {'print $2'} | xargs kill -9 >>$LOGFILE
+	sleep 1
+	
+	DO_WHEREISPROGRM=`whereis $I`
+        echo "whereis progrm === $DO_WHEREISPROGRM" >>$LOGFILE
+
+        DO_FILE_PATH=`echo $DO_WHEREISPROGRM | awk '{print $2}'`
+        echo "do program is ===>$DO_FILE_PATH" >>$LOGFILE
+
+
+       	sudo chmod 644 $DO_FILE_PATH
+
+      	PROGRM_CHMOD=`ls -al "$DO_FILE_PATH" | awk '{print $1}'`
+       	echo "PROGRM_CHMOD===$PROGRM_CHMOD" >>$LOGFILE
+
+
+        INSRET=$INSRET"{\"debname\":\"${I}\",\"state\":\"$PROGRM_CHMOD\"}"
+
+        if [ "$EC" -eq "$#" ]; then
+                INSRET=$INSRET","
+        fi
+
+        EC=`expr "$EC" + 1`
+
+
+
+done;
+fi
+
+
+
+
+echo "================================================================="
+echo "################### updt deb install ###########################"
+
+UPDT_INS=`cat /etc/hamonize/progrm/progrm.hm | jq '.DEL' | sed -e "s/\"//g" `
+echo "install file total  data=========$UPDT_INS" >>$LOGFILE
+
+if [ "$UPDT_INS" != null ]
+then
+        echo "program access possible ==" >>$LOGFILE
+
+EC=0
+DELRET=""
+OLD_IFS=$IFS;
+IFS=,;
+for I in $UPDT_INS;
+do
+
+
+        echo "program name is ===> $I" >>$LOGFILE
+
+
+	DO_WHEREISPROGRM=`whereis $I`
+        echo "whereis progrm === $DO_WHEREISPROGRM" >>$LOGFILE
+
+       	DO_FILE_PATH=`echo $DO_WHEREISPROGRM | awk '{print $2}'`
+        echo "do program is ===>$DO_FILE_PATH" >>$LOGFILE
+
+       	sudo chmod 755 $DO_FILE_PATH
+       
+       	PROGRM_CHMOD=`ls -al "$DO_FILE_PATH" | awk '{print $1}'`
+       	echo "PROGRM_CHMOD===$PROGRM_CHMOD"
+
+
+
+        DELRET=$DELRET"{\"debname\":\"${I}\",\"state\":\"$PROGRM_CHMOD\"}"
+
+        if [ "$EC" -eq "$#" ]; then
+                DELRET=$DELRET","
+        fi
+
+        EC=`expr "$EC" + 1`
+
+
+
+done;
+fi
+
+
+
+
+echo "################## updt json data ############################"
+
+UPDT_JSON="{\
+       \"insresert\":[$INSRET],\
+       \"delresert\": [$DELRET],\
+       \"uuid\": \"$PCUUID\"\
+}"
+
+
+echo $UPDT_JSON >>$LOGFILE
+
+
+#RETUPDT=`curl  -X  POST -H 'User-Agent: HamoniKR OS' -H 'Content-Type: application/json' -f -s -d "$UPDT_JSON" $CENTERURL`
+
+#echo $RETUPDT >> ${LOGFILE}
+
+
