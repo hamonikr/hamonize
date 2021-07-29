77d237c038971f018a64516e9e54a6d6c3531709
eden <swkim@invesume.com>
2021-07-29 19:03:25 +0900

    db connection 방식 변경

---
+++ hamonize-center/src/main/java/com/PsqlDatabaseConfig.java
@@ -0,0 +1,57 @@
+package com;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@MapperScan(value="com.mapper")
+@EnableTransactionManagement
+public class PsqlDatabaseConfig {
+
+    @Autowired
+    GlobalPropertySource globalPropertySource;
+
+    @Bean
+    @Primary
+    public DataSource customDataSource() {
+        return DataSourceBuilder
+            .create()
+            .url(globalPropertySource.getUrl())
+            .driverClassName(globalPropertySource.getDriverClassName())
+            .username(globalPropertySource.getUsername())
+            .password(globalPropertySource.getPassword())
+            .build();
+    }
+
+    // @Bean(name = "db1DataSource")
+	// @ConfigurationProperties(prefix = "spring.db1.datasource")
+	// public DataSource db1DataSource() {
+	// 	return DataSourceBuilder.create().build();
+	// }
+
+   @Bean
+   public SqlSessionFactory db1SqlSessionFactory(ApplicationContext applicationContext) throws Exception {
+       SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+       sqlSessionFactoryBean.setDataSource(customDataSource());
+       sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:publicMapper/*.xml"));
+       
+       return sqlSessionFactoryBean.getObject();
+   }
+
+   @Bean
+   public SqlSessionTemplate db1SqlSessionTemplate(SqlSessionFactory db1SqlSessionFactory) throws Exception {
+       return new SqlSessionTemplate(db1SqlSessionFactory);
+   }
+}
