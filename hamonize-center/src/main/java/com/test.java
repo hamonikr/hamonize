c69ff687407db7aecbbaebc642c2d7745267b03c
jullee <bono623@naver.com>
2021-08-11 19:31:15 +0900

    #115 센터 리드미 수정

---
+++ hamonize-center/src/main/java/com/test.java
@@ -0,0 +1,12 @@
+package com;
+
+public class test {
+    private String aa = "aaa";
+
+    public test() {
+        if (aa != "aaa")
+        {            System.out.println("!aa");
+        } else {System.out.println("!aa")}
+    }
+
+}
