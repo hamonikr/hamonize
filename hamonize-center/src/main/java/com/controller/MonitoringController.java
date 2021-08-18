6eccba046ff9d7b53fbf532b56dc22c51dd55ff4
jullee <bono623@naver.com>
2021-08-18 17:13:14 +0900

    :boom: #141 이슈 수정

--- hamonize-center/src/main/java/com/controller/MonitoringController.java
+++ hamonize-center/src/main/java/com/controller/MonitoringController.java
@@ -68,11 +68,12 @@ public class MonitoringController {
 		try {
 			list = mService.pcListInfo(params);
 			for (int i = 0; i < list.size(); i++) {
-				
-				//if (list.get(i).get("pc_status") != null && list.get(i).get("pc_status") != "" ) {
-				if(!ObjectUtils.isEmpty(list.get(i).get("pc_status"))) {
+
+				// if (list.get(i).get("pc_status") != null && list.get(i).get("pc_status") != "" )
+				// {
+				if (!ObjectUtils.isEmpty(list.get(i).get("pc_status"))) {
 					on++;
-				}else {
+				} else {
 					off++;
 				}
 			}
@@ -80,10 +81,10 @@ public class MonitoringController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println("on==={"+ on);
-		System.out.println("off==={"+ off);
-		
+
+		System.out.println("on==={" + on);
+		System.out.println("off==={" + off);
+
 		result.put("pcList", list);
 		result.put("on", on);
 		result.put("off", off);
@@ -95,13 +96,14 @@ public class MonitoringController {
 	}
 
 	/**
-	 * 로그감사 > 정책배포결과 조직도 클릭시 해당 부서/팀에 소속된 pc리스트와 적용된 정책리스트 출력 출력 사항 : 업데이트 배포 결과,
-	 * 프로그램 차단 배포 결과, 방화벽 정책 배포결과, 디바이스 정책배포결과
+	 * 로그감사 > 정책배포결과 조직도 클릭시 해당 부서/팀에 소속된 pc리스트와 적용된 정책리스트 출력 출력 사항 : 업데이트 배포 결과, 프로그램 차단 배포 결과, 방화벽
+	 * 정책 배포결과, 디바이스 정책배포결과
 	 * 
 	 */
 	@ResponseBody
 	@RequestMapping(value = "/pcPolicyList")
-	public HashMap<String, Object> pcPolicyList(Model model, @RequestParam Map<String, Object> params) {
+	public HashMap<String, Object> pcPolicyList(Model model,
+			@RequestParam Map<String, Object> params) {
 		HashMap<String, Object> jsonObject = new HashMap<String, Object>();
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
