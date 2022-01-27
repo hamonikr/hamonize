42f27a9c19f6dd77be8eae87e9296551224b0961
Eden <togoforhere@gmail.com>
2022-01-27 18:10:28 +0900

    <Feat> : weclient testfile add

--- hamonize-center/src/main/java/com/util/Test.java
+++ hamonize-center/src/main/java/com/util/Test.java
@@ -1,11 +1,13 @@
 package com.util;
 
+import org.apache.http.entity.ContentType;
 import org.json.simple.JSONArray;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Flux;
@@ -21,10 +23,8 @@ public class Test {
       WebClient wc = WebClient.builder().baseUrl("http://192.168.0.212").build();
 
       try {
-        ResponseEntity<JSONArray> response = wc.get().uri(uri -> uri.path("/api/v2/inventories/")
-        .queryParam("username", "admin")
-        .queryParam("password", "password")
-        .build())
+        //ResponseEntity<JSONArray> response = wc.get().uri(uri -> uri.pathSegment("/api/v2/inventories/","{id}")
+        ResponseEntity<JSONArray> response = wc.get().uri("/api/v2/inventories/{id}",1)
         .accept(MediaType.APPLICATION_JSON)
         .retrieve()
         .toEntity(JSONArray.class).block();
@@ -44,13 +44,22 @@ public class Test {
       try {
         System.out.println("11111111111111111111111111111111");
         Mono<String> response = wc.get().uri(UriBuilder -> UriBuilder
-        .path("/api/v2/inventories/")
+        .path("/api/v2/inventories/").path("{id}/").path("inventory_sources/")
         //.queryParam("--user", "admin:password")
         //.queryParam("password", "password")
-        .build())
-        .accept(MediaType.APPLICATION_JSON)
-        .retrieve()
-        .bodyToMono(String.class);
+        .build(3))
+        .exchange().flatMap(clientResponse -> {
+          if (clientResponse.statusCode().is5xxServerError()) {
+              clientResponse.body((clientHttpResponse, context) -> {
+                  return clientHttpResponse.getBody();
+              });
+              return clientResponse.bodyToMono(String.class);
+          }
+          else
+              return clientResponse.bodyToMono(String.class);
+      });
+        //.accept(MediaType.APPLICATION_JSON)
+        //.retrieve()
         //.bodyToMono(String.class);
         // Mono<String> response = wc.get() 
         // .uri("/api/v2/inventories/")
@@ -71,22 +80,74 @@ public class Test {
 
       WebClient wc = WebClient.builder()
       .defaultHeaders(header -> header.setBasicAuth("admin","password"))
+      .baseUrl("http://192.168.0.212")
+      .build();
+      
+      try {
+        String request = "{\"name\": \"invesume2\",\"description\": \"invesume\",\"source\": \"custom\"}";
+        LinkedMultiValueMap map = new LinkedMultiValueMap();
+        map.add("data", request);
+        Mono<String> response = wc.post()
+        .uri(UriBuilder -> UriBuilder
+        .path("/api/v2/inventories/").path("{id}/").path("inventory_sources/")
+        .build(3))
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(request))
+        //에러 확인
+        .exchange().flatMap(clientResponse -> {
+          if (clientResponse.statusCode().is5xxServerError()) {
+              clientResponse.body((clientHttpResponse, context) -> {
+                  return clientHttpResponse.getBody();
+              });
+              return clientResponse.bodyToMono(String.class);
+          }
+          else
+              return clientResponse.bodyToMono(String.class);
+      });
+        //.bodyValue(request)
+        //.accept(MediaType.APPLICATION_JSON)
+        //.retrieve()
+        //.bodyToMono(String.class); 
+
+        String objects = response.block();
+        return objects.toString();
+      } catch (Exception e) {
+        //TODO: handle exception
+      }
+      return null;
+  
+    }
+
+    @RequestMapping(value="test3")
+    public String addHosts(){
+
+      WebClient wc = WebClient.builder()
+      .defaultHeaders(header -> header.setBasicAuth("admin","password"))
       .baseUrl("http://192.168.0.212").build();
       
       try {
-        String request = "{\"name\":\"invesume\",\"organization\":\"1\",\"description\":\"invesume hostlist\"}";
-        LinkedMultiValueMap map = new LinkedMultiValueMap();
-        map.add("request", request);
+        String request = "{\"name\": \"invesume\",\"description\": \"invesume hostlist\",\"variables\": \"{'hosts': ['192.168.0.225'], 'vars': {'ansible_user': 'hamonikr','ansible_pass': '0745'}}\"}";
+        //"\thosts:\n"+
+        //"\t\t192.168.0.225}\"";
+        System.out.println("request====="+request);
         Mono<String> response = wc.post().uri(UriBuilder -> UriBuilder
-        .path("/api/v2/inventories/")
-        // .queryParam("name", "invesume")
-        // .queryParam("organization", "1")
-        // .queryParam("description", "invesumehost")
-        .build())
-        .body(map)
-        .accept(MediaType.APPLICATION_JSON)
-        .retrieve()
-        .bodyToMono(String.class); 
+        .path("/api/v2/inventories/").path("{id}").path("/hosts/")
+        .build(3))
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(request))
+        .exchange().flatMap(clientResponse -> {
+          if (clientResponse.statusCode().is5xxServerError() || clientResponse.statusCode().isError() || clientResponse.statusCode().is4xxClientError()) {
+              clientResponse.body((clientHttpResponse, context) -> {
+                  return clientHttpResponse.getBody();
+              });
+              return clientResponse.bodyToMono(String.class);
+          }
+          else
+              return clientResponse.bodyToMono(String.class);
+      });
+        //.accept(MediaType.APPLICATION_JSON)
+        //.retrieve()
+        //.bodyToMono(String.class); 
 
         String objects = response.block();
         return objects.toString();
