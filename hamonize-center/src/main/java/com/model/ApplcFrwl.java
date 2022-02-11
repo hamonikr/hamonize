014c630bca5206e4668e56cebeb041c9aa4b1e55
Eden <togoforhere@gmail.com>
2022-02-11 18:22:45 +0900

    <Feat> : 정책 프로그램,방화벽,디바이스 jpa 변환

---
+++ hamonize-center/src/main/java/com/model/ApplcFrwl.java
@@ -0,0 +1,35 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Comment;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="tbl_frwl_applc")
+public class ApplcFrwl {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Comment("시리얼넘버")
+	private Long seq;
+
+	@Size(max=50)
+	private String domain;
+
+	private Integer org_seq;
+	@Size(max=500)
+	private String ppm_seq;
+
+}
