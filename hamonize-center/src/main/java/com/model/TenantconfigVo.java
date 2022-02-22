01625ee5e414499555dab3d48350fefedcfa882e
Eden <togoforhere@gmail.com>
2022-02-22 18:24:10 +0900

    <Feat> : jpa 설정

--- hamonize-center/src/main/java/com/model/TenantconfigVo.java
+++ hamonize-center/src/main/java/com/model/TenantconfigVo.java
@@ -2,7 +2,13 @@ package com.model;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.Comment;
@@ -14,19 +20,45 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name="tbl_tenant_config",uniqueConstraints={
+	@UniqueConstraint( columnNames={"domain"})
+	})
 public class TenantconfigVo {
+	
 
-	private Long seq;
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Comment("시리얼번호")
+	private Long seq;
+
 	@Size(max=50)
-	@Comment("")
+	@Comment("테넌트")
+	@Column(nullable = false)
 	private String domain;
-	private Integer tenant_file_seq;
+
+	@Comment("")
+	private Long tenant_file_seq;
+
+	@Column(columnDefinition="TEXT")
+	@Comment("")
 	private String tenant_hadmin_config;
+
+	@Column(columnDefinition="TEXT")
+	@Comment("")
 	private String tenant_hadmin_public_key;
+
+	@Column(columnDefinition="TEXT")
+	@Comment("")
 	private String tenant_hadmin_private_key;
+
 	private Integer tenant_polling_time;
-	private String tenant_authkey;	
+
+	private String tenant_authkey;
+	
+	@Size(max=4)
+	private String tenant_vpn_used;
+
 	private Timestamp rgstr_date;
 	private Timestamp updt_date;
 	
