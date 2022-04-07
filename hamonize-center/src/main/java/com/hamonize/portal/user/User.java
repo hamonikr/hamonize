9d6cf6637831ac8ab7b1345f57b05f24038f25c5
jullee <bono623@naver.com>
2022-04-07 13:18:14 +0900

    <Fix> : redis 로그인 오류 수정

--- hamonize-center/src/main/java/com/hamonize/portal/user/User.java
+++ hamonize-center/src/main/java/com/hamonize/portal/user/User.java
@@ -1,10 +1,13 @@
 package com.hamonize.portal.user;
 
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -13,6 +16,7 @@ import javax.persistence.Transient;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.Comment;
+import org.jetbrains.annotations.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -22,27 +26,34 @@ import lombok.Setter;
 @Entity
 @Table(name="tbl_admin_user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long seq;
+
 	@Size(max=50)
 	@Comment("도메인 명")
 	private String domain;
 
-    @Id
-	@Size(max=50)
-	@Column(name = "user_id")
+    @Column(name = "user_id")
 	@Comment("유저 아이디")
 	private String userid;
 
+	@NotNull
 	@Column(name = "pass_wd")
     @Size(max=300)
 	@Comment("유저 패스워드")
 	private String passwd;
 
+	@Transient
+	private String beforepasswd;
+	
 	@Column(name = "user_name")
     @Size(max=50)
 	@Comment("유저 이름")
 	private String username;
 
-    @Size(max=50)
+	@Size(max=50)
 	@Comment("유저 이름")
 	private String email;
 
@@ -52,24 +63,23 @@ public class User {
 	@Comment("활성 비활성 구분 값 > 1 : 활성 , 0 : 비활성")
 	private String status;
 	
-	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "rgstr_date")
-	private Date rgstrDate;
+	private LocalDateTime rgstrdate;
 
-	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "updt_date")
-	private Date updtDate;
+	private LocalDateTime updtdate;
 	
-    @Column(name="login_check",columnDefinition = "integer default 0")
+    @Transient
+	@Column(name="login_check",columnDefinition = "integer default 0")
 	private int logincheck;
-
-	@Comment("프로필 이미지")
-    @Size(max=300)
-	private String picture;
 	
-	@Transient
 	@Comment("유저권한 포탈 유저 : USER / 어드민 유저 : ADMIN ")
 	private String role;
 
+	@Comment("이메일 인증키")
+	private String authkey;
+
+	@Comment("소셜 이미지")
+	private String picture;
 		
 }
